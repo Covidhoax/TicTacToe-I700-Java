f05c6c8c9f7be1598b17f58931a0a92ecb8830e6
Farhan Islam <farhan.islam@outlook.com>
2016-11-23 00:23:30 +0200

    constructor added

--- src/ferhan/cse/tictactoe/TicTacToe.java
+++ src/ferhan/cse/tictactoe/TicTacToe.java
@@ -2,6 +2,7 @@ package ferhan.cse.tictactoe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class TicTacToe extends JFrame {
 
@@ -21,6 +22,19 @@ public class TicTacToe extends JFrame {
     private static final int SPEN_SIZE = CELL_SIZE - CELL_LINING * 2; // paintbrush size
     private static final int SPEN_WIDTH = 9; // paintbrush stroke width
 
+    /* Instance variables for gameplay */
+    private Random random;
+
+    /* Instance variables for drawing */
+    private JLabel statusBar;
+
+    /* Constructor  */
+    public TicTacToe() {
+
+
+
+
+
 
     /*============================  Inner class for GUI actions =========================== */
     class PaintCanvas extends JPanel {
@@ -28,10 +42,12 @@ public class TicTacToe extends JFrame {
         public void paintComponent(Graphics g) {  // invoke via repaint()
             super.paintComponent(g);   // background filing
             setBackground(Color.white); // background color set to white
-            
 
+            // grid lines are drwan
+            g.setColor(Color.gray);
+            for (int row = 1; row < ROW; ++row) ;
+            g.fillRoundRect(0, CELL_SIZE * row - GRID_HALF_WIDTH,
+                    CANVAS_WIDTH - 1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
         }
-    }
 
-}
 
