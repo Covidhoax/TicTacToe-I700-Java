a4acfa4c63e950fab9dcf6a472e77aed136bfd07
Farhan Islam <farhan.islam@outlook.com>
2016-11-22 16:48:05 +0200

    Modifiers are changed to private

--- src/ferhan/cse/tictactoe/TicTacToe.java
+++ src/ferhan/cse/tictactoe/TicTacToe.java
@@ -4,8 +4,20 @@ import javax.swing.*;
 
 public class TicTacToe extends JFrame {
 
-    /*====================== Instance variables ======================= */
-    public static final int ROW = 3;  // ROW by COLUMN cells
-    public static final int COLUMN = 3;
+    /*====================== Instance variables constants ======================= */
+    private static final int ROW = 3;  // ROW by COLUMN cells
+    private static final int COLUMN = 3;
+
+    /*Variable using differemt dimensions and shall be used for GUI */
+    private static final int CELL_SIZE = 125; // cell width and height
+    private static final int CANVAS_WIDTH = CELL_SIZE * COLUMN;  // the drawing canvas
+    private static final int CANVAS_HEIGHT = CELL_SIZE * ROW;
+    private static final int GRID_WIDTH = 9;                   // Grid-line's width
+    private static final int GRID_HALF_WIDTH = GRID_WIDTH / 2; // Grid-line's half-width
+
+    /* Xs and Ys are displayed inside of the cell with lined border  */
+    private static final int CELL_LINING = CELL_SIZE / 7;
+    private static final int SPEN_SIZE = CELL_SIZE - CELL_LINING * 2; // paintbrush size
+    private static final int SPEN_WIDTH = 9 ; // paintbrush stroke width
 
 }
