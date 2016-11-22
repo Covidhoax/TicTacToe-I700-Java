7d553212da747d8f2349145fec3d38322b9d6f34
Farhan Islam <farhan.islam@outlook.com>
2016-11-22 16:30:01 +0200

    instance variables added

---
+++ src/ferhan/cse/tictactoe/TicTacToe.java
@@ -0,0 +1,11 @@
+package ferhan.cse.tictactoe;
+
+import javax.swing.*;
+
+public class TicTacToe extends JFrame {
+
+    /*====================== Instance variables ======================= */
+    public static final int ROW = 3;  // ROW by COLUMN cells
+    public static final int COLUMN = 3;
+
+}
