ad67dca327bf9d3ef28dca97243e7d0503f0469b
Farhan Islam <farhan.islam@outlook.com>
2016-11-24 01:04:04 +0200

    GameState and Player classes

---
+++ src/ferhan/cse/tictactoe/GameState.java
@@ -0,0 +1,7 @@
+package ferhan.cse.tictactoe;
+
+/**
+ * Created by Farhan on 11/24/2016.
+ */
+public enum GameState {
+}
---
+++ src/ferhan/cse/tictactoe/Player.java
@@ -0,0 +1,8 @@
+package ferhan.cse.tictactoe;
+
+/**
+ * Created by Farhan on 11/24/2016.
+ */
+public enum  Player {
+    EMPTY
+}
