c8e22b9241d984a09bd7bc40b7c02215c7f43253
Farhan Islam <farhan.islam@outlook.com>
2016-11-26 02:23:47 +0200

    BgMusic added. Theme music play in infinite loop

---
+++ src/ferhan/cse/tictactoe/BgMusic.java
@@ -0,0 +1,36 @@
+package ferhan.cse.tictactoe;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.swing.*;
+import java.io.File;
+
+/**
+ * Created by Farhan on 11/26/2016.
+ */
+public class BgMusic extends JFrame{
+
+    public BgMusic () {
+
+        playMusic ();
+    }
+
+    private void playMusic() {
+        try {
+            //http://www.vgmusic.com/music/other/miscellaneous/arcade/1944_jungle.mid//
+            Sequence sequence = MidiSystem.getSequence(new File("music/ttt.mid"));
+
+            // Sequencer for music
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.setSequence(sequence);
+
+            // midi plays in a infinite loop
+            sequencer.start();
+            sequencer.setLoopCount(Integer.MAX_VALUE);
+        } catch (Exception exception) {
+
+        }
+    }
+}
--- src/ferhan/cse/tictactoe/Test.java
+++ src/ferhan/cse/tictactoe/Test.java
@@ -4,7 +4,8 @@ package ferhan.cse.tictactoe;
  * Created by Farhan on 11/26/2016.
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        BgMusic midi = new BgMusic();
         TicTacToe ticTacToeFr = new TicTacToe();
     }
 }
