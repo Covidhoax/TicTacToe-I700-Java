587562919305b50b2458c4aa0f4f2e464c73701b
Farhan Islam <farhan.islam@outlook.com>
2016-11-26 02:12:06 +0200

    Main method added.Game works

---
+++ src/ferhan/cse/tictactoe/Test.java
@@ -0,0 +1,10 @@
+package ferhan.cse.tictactoe;
+
+/**
+ * Created by Farhan on 11/26/2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        TicTacToe ticTacToeFr = new TicTacToe();
+    }
+}
